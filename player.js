4a9abd7d666f04113426964d846041d4f3a369ef
SteuerSchleuder1000 <aregger.pascal@gmail.com>
2017-11-03 11:31:18 +0100

    Update player.js
    decks

--- player.js
+++ player.js
@@ -21,7 +21,7 @@ class Player {
         this.options.x = 0
         this.options.y = 0
         this.options.player = this
-        let dl = choice([DL_Rust, DL_Thunder, DL_Star])//[DL_Thunder,DL_Snake,DL_Star,DL_Rust])
+        let dl = choice([DL_Rust])//[DL_Thunder,DL_Snake,DL_Star,DL_Rust])
 
         
         let count = 0
